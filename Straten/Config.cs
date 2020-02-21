f21ec7595d0e51c3ce070d80c6bd2f9544ae59a6
Luc Vervoort <luc.vervoort@hogent.be>
2020-02-21 12:11:43 +0100

    Straten config.cs

---
+++ Straten/Config.cs
@@ -0,0 +1,13 @@
+﻿namespace Straten
+{
+    public static class Config
+    {
+        public static string TaalCode { get; set; } = "nl";
+        public static string Path { get; set; } = "C:/Users/u2389/source/repos/lucvervoort/Exercises/Straten/repository";
+        public const string StraatnaamGemeenteId = "StraatnaamID_gemeenteID.csv";
+        public const string Straatnamen = "WRstraatnamen.csv";
+        public const string ProvincieIds = "ProvincieIdsVlaanderen.csv";
+        public const string ProvincieInfo = "ProvincieInfo.csv";
+        public const string Gemeentenamen = "WRGemeentenaam.csv";
+    }
+}
