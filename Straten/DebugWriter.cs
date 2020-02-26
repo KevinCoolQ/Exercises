82a22190481789de393adfb1dcd1635252859bbf
Luc Vervoort <luc.vervoort@hogent.be>
2020-02-26 09:54:02 +0100

    DebugWriter

---
+++ Straten/DebugWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Straten
+{
+    public static class DebugWriter
+    {
+        [Conditional("DEBUG")]
+        public static void WriteLine(string m)
+        {
+            System.Console.WriteLine(m);
+        }
+    }
+}
