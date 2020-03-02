3d514583a2e901e7685eef18b8df261874ab8ffe
Luc Vervoort <luc.vervoort@hogent.be>
2020-03-02 12:19:28 +0100

    linqpad

---
+++ LinqInDePraktijkSolutions/LinqPad/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqPad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("https://www.linqpad.net/");
+            Console.WriteLine("Read document: integrate your own linq sample set");
+            Console.WriteLine("Load set of 50 examples");
+        }
+    }
+}
